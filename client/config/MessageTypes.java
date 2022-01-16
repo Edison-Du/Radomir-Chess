308a2ed51c3aaaf41a8a680977f73d42c3bfe054
Edison-Du <duedison794@gmail.com>
2022-01-16 00:25:44 -0500

    Edge case for full games

--- client/config/MessageTypes.java
+++ client/config/MessageTypes.java
@@ -16,6 +16,7 @@ public class MessageTypes {
     public static final String JOINED_GAME = "JOINED_GAME";
     public static final String CREATE_GAME = "CREATE_GAME";
     public static final String GAME_CREATED = "GAME_CREATED";
+    public static final String GAME_FULL = "GAME_FULL";
 
     public static final String GUEST_JOINED = "GUEST_JOINED";
     public static final String SENT_TEXT = "SENT_TEXT";
